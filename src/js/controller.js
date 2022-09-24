b7e39413ae93f5d862bdcfce4efef6090ba158fa
Premjeet1804 <cs2054@global.org.in>
2022-09-24 14:40:48 +0530

    Added new welcome features

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(constrolBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,6 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('welcome !');
+  newFeature();
 };
 init();
